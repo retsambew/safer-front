4f8c47234a80b54ecf5814b13e9549d2e0a98694
retsambew <namanaskn2@gmail,com>
2022-08-24 23:24:09 +0530

    signup-login

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import {Text, View} from 'react-native'
+import React from "react";
+import { Text, View } from "react-native";
 
-const Home = () => {
+const Home = ({ navigation }) => {
   return (
-    <View style={{
-        flex:1, justifyContent:'center', alignItems:'center'
-    }}>
-        <Text>Home Page</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Home Page</Text>
     </View>
-  )
-}
+  );
+};
 
 export default Home;
--- src/components/Navigation/MainNav.js
+++ src/components/Navigation/MainNav.js
@@ -1,20 +1,20 @@
-import * as React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from '../Home/index.js';
-import Login from '../auth/login.js';
-import SignUp from '../auth/signup';
-import Upload from '../Upload';
+import * as React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../Home/index.js";
+import Login from "../auth/login.js";
+import SignUp from "../auth/signup";
+import Upload from "../Upload";
 
 const MainNav = () => {
-    const Stack = createNativeStackNavigator();
-    return (
-        <Stack.Navigator initialRouteName="SignUp">
-            <Stack.Screen name="Home" component={Home} headerShown={false} />
-            <Stack.Screen name="Login" component={Login} headerShown={false} />
-            <Stack.Screen name='SignUp' component={SignUp} headerShown={false} />
-            <Stack.Screen name="Upload" component={Upload} headerShown={false} />
-        </Stack.Navigator>
-    )
-}
+  const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator initialRouteName="Login" headersMode="none">
+      <Stack.Screen name="Home" component={Home} headerShown={false} />
+      <Stack.Screen name="Login" component={Login} headerShown={false} />
+      <Stack.Screen name="SignUp" component={SignUp} headerShown={false} />
+      <Stack.Screen name="Upload" component={Upload} headerShown={false} />
+    </Stack.Navigator>
+  );
+};
 
-export default MainNav
+export default MainNav;
